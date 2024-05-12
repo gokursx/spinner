4a67b1c849e12a543c5e1c80239e2b9bca295654
Gokul Krishna <53399680+gokursx@users.noreply.github.com>
2024-05-12 10:05:31 -0700

    Update spinner1.js

--- spinner1.js
+++ spinner1.js
@@ -1,20 +1,12 @@
+const spinner = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|'];
+
+const animateSpinner = (index) => {
+  process.stdout.write(`\r${spinner[index]}   `);
+  setTimeout(() => {
+    index = (index + 1) % spinner.length;
+    animateSpinner(index);
+  }, 200);
+};
+
 process.stdout.write('hello from spinner1.js... \rheyyy\n');
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
-}, 700);
-
-// ... fill in the rest yourself ...
+animateSpinner(0);
